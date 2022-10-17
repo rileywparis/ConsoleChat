93f9ed1d3b36e325cc2edce10a793035cbb357d9
Riley Paris <RWParis@student.fullsail.edu>
2022-10-16 21:36:49 -0400

    Add project files.

---
+++ ChatWindow/Chat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWindow
+{
+    internal class Chat
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int PORT = 25565;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Chat Window";
+            ClientSocket.Connect(IPAddress.Loopback, PORT);
+            Console.Clear();
+            while (true)
+                ReceiveMessage();
+        }
+
+        private static void ReceiveMessage()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Client/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Client
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 25565;
+
+        static string name = "user";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Console.SetWindowSize(80, 5);
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    Console.WriteLine("Trying to connect");
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Connected");
+            Console.WriteLine("Enter username: ");
+            name = Console.ReadLine();
+            Console.Clear();
+
+            ProcessStartInfo p = new ProcessStartInfo(@"C:\Users\panda\source\repos\ConsoleChat\ChatWindow\bin\Debug\ChatWindow.exe");
+            Process.Start(p);
+
+            while (true)
+                SendMessage();
+        }
+
+        private static void SendMessage()
+        {
+            Console.Write("Type: ");
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(name + ": " + message);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+            Console.Clear();
+        }
+    }
+}
