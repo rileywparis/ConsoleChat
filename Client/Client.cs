924f2794784cdcad6cbcb6aafbc85fdc3524caed
Riley Paris <RWParis@student.fullsail.edu>
2022-10-17 23:18:20 -0400

    final demo

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -12,26 +11,30 @@ namespace Client
 {
     internal class Client
     {
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         private const int PORT = 25565;
-        private static string PATH = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AppServer\Upload";
+        static string name = "user";
 
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(PATH);
             Console.Title = "Client";
             Console.SetWindowSize(80, 5);
-            //while (!clientSocket.Connected)
-            //    try
-            //    {
-            //        Console.WriteLine("Trying to connect");
-            //        clientSocket.Connect("172.20.8.252", PORT);
-            //    }
-            //    catch (SocketException)
-            //    {
-            //        Console.Clear();
-            //    }
+            while (!ClientSocket.Connected)
+                try
+                {
+                    Console.WriteLine("Trying to connect");
+                    ClientSocket.Connect("172.20.8.252", PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
             Console.Clear();
             Console.WriteLine("Connected");
+            Console.WriteLine("Enter username: ");
+            name = Console.ReadLine();
+            Console.Clear();
+            Process.Start(@"C:\Users\panda\source\repos\ConsoleChat\ChatWindow\bin\Debug\ChatWindow.exe");
 
             while (true)
                 SendMessage();
@@ -41,25 +44,8 @@ namespace Client
         {
             Console.Write("Type: ");
             string message = Console.ReadLine();
-            if (message == "send")
-            {
-                foreach (string f in Directory.GetFiles(PATH))
-                {
-                    Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                    byte[] fileNameByte = Encoding.ASCII.GetBytes(f);
-                    byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
-                    byte[] fileData = File.ReadAllBytes(f);
-                    byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
-
-                    fileNameLen.CopyTo(clientData, 0);
-                    fileNameByte.CopyTo(clientData, 4);
-                    fileData.CopyTo(clientData, 4 + fileNameByte.Length);
-                    clientSocket.Connect("172.20.8.252", PORT);
-                    clientSocket.Send(clientData);
-                    clientSocket.Close();
-                }
-            }
+            byte[] buffer = Encoding.ASCII.GetBytes(name + ": " + message);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
             Console.Clear();
         }
     }
--- ConsoleChat/Server.cs
+++ ConsoleChat/Server.cs
@@ -1,51 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleChat
 {
     internal class Server
     {
-        private static List<Socket> clientSockets = new List<Socket>();
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
         private const int PORT = 25565;
-        private static string PATH = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AppServer";
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
 
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(PATH);
             Console.Title = "Server";
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Setting up server...");
             serverSocket.Bind(new IPEndPoint(IPAddress.Parse("172.20.8.252"), PORT));
-            serverSocket.Listen(PORT);
-            Console.WriteLine("Server Started");
-
-            while (true)
-            {
-                Socket clientSocket = serverSocket.Accept();
-                new Thread(delegate ()
-                {
-                    Receive(clientSocket);
-                }).Start();
-            }
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine("Setup complete");
+            Console.ReadLine();
         }
 
-        public static void Receive(Socket clientSocket)
+        private static void AcceptCallback(IAsyncResult AR)
         {
-            byte[] clientData = new byte[1024 * 5000];
-            int receivedBytesLen = clientSocket.Receive(clientData);
-            int fileNameLen = BitConverter.ToInt32(clientData, 0);
-            string fileName = new DirectoryInfo(Encoding.ASCII.GetString(clientData, 4, fileNameLen)).Name;
-            Console.WriteLine(clientSocket.LocalEndPoint + " sending " + fileName);
-            BinaryWriter bWrite = new BinaryWriter(File.Open(PATH + @"\" + fileName, FileMode.Create));
-            bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
-            bWrite.Close();
-            clientSocket.Close();
+            Socket socket;
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client disconnected");
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine(text);
+
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            foreach (Socket s in clientSockets)
+                if (s != current)
+                    s.Send(data);
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
         }
     }
 }
